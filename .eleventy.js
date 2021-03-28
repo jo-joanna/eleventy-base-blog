ce7f1270213502e79c39560deb09f70d4734c085
jo-joanna <b.joanna.uk@gmail.com>
2021-03-28 20:17:22 +0200

    added YML to template classes

--- .eleventy.js
+++ .eleventy.js
@@ -104,7 +104,8 @@ module.exports = function(eleventyConfig) {
       "md",
       "njk",
       "html",
-      "liquid"
+      "liquid",
+      "yml"
     ],
 
     // If your site lives in a different subdirectory, change this.
